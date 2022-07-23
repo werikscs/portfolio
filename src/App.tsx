05e0242c49925b4c9961394dbc7bf0367417c7be
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-23 20:27:39 -0300

    refactor: change app function to arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import "./styles/_main.scss";
 import Routers from "./routes";
 import Header from "./components/Header";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Header />
       <Routers />
     </div>
   );
-}
+};
 
 export default App;
