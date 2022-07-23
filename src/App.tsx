702229870edb7f2b7d862381b6f5b898a94db889
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-07-23 20:26:38 -0300

    feat: add routers and nav component to app function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import "./styles/_main.scss";
+
+import Routers from "./routes";
+import Header from "./components/Header";
+
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routers />
+    </div>
+  );
 }
 
 export default App;
